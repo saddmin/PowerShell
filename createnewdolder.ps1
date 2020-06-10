d4a8842a316e30a2653a4480754b5acec68f6524
saddmin <55796126+saddmin@users.noreply.github.com>
2020-06-10 17:58:00 -0400

    Add files via upload

---
+++ createnewdolder.ps1
@@ -0,0 +1,9 @@
+﻿If (-not (Test-Path $env\scriptoutputs))
+    {
+        New-Item -Path $env\scriptoutputs -ItemType "directory"
+
+        Set-Content -Path $env\scriptoutputs\created.txt -Value "this directory was created"
+
+        Get-Date -Format g | 
+        Add-Content -Path $env\scriptoutputs\created.txt
+    }
